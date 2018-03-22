ad7cc496f2ac0c5780b94a141cf639c1d0715b75
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-03-22 18:25:32 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -18,7 +18,7 @@ class App < Sinatra::Base
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
     #params[:word1] + " " params[:word2] + params[:word3] + params[:word4] + params[:word5]
-    params.values.join(" ")
+    params.values.join(" ") + "."
   end
 
 
