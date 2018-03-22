a649f8c49eb1cbe6d34d4f35c009ac176a1b16dc
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-03-22 18:24:28 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -12,9 +12,9 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase' do
-    params[:number].to_i * params[:phrase]
+    params[:number].to_i + 1 * params[:phrase]
     end
-  
+
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
     params[:word1] + params[:word2] + params[:word3] + params[:word4] + params[:word5]
