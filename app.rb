03094f6423941872a638cc6023c96509ffe5f20b
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-03-22 18:06:44 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,10 +4,10 @@ class App < Sinatra::Base
   # Write your code here!
   get '/reversename/:name' do
     params[:name].reverse
-  end 
+  end
 
-  get '/square/:number' do 
-    @sqaure = params[:number].to_i * params[:number].to_i 
-    @square.to_s 
+  get '/square/:number' do
+    @sqaure = params[:number].to_i * params[:number].to_i
+    @square.to_s
   end 
 end
